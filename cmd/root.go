ca143506256e2eb564331f6473ccbf24fad28498
Doron Cohen <doroncohen@fastmail.com>
2020-09-19 20:01:08 +0300

    feat: add version flag

--- cmd/root.go
+++ cmd/root.go
@@ -11,11 +11,11 @@ var rootCmd = &cobra.Command{
 	Use:   "antidot",
 	Short: "Clean your $HOME from those pesky dotfiles",
 	Run: func(cmd *cobra.Command, args []string) {
-
 	},
 }
 
-func Execute() {
+func Execute(appVersion string) {
+	rootCmd.Version = appVersion
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"github.com/doron-cohen/antidot/cmd"
 )
 
+/*
+We are injecting the app's version to this variable.
+This is done using: go build -ldflags "-X 'main.version=$VERSION".
+It is only used in the cmd package but I couldn't make it inject there.
+*/
+var version = "dev"
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(version)
 }
