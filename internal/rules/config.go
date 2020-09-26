fdfc4d0c6ba3a22660857b9a9aa2348c462a2670
Doron Cohen <doroncohen@fastmail.com>
2020-09-26 15:16:44 +0300

    refactor: return config from load function

--- internal/rules/config.go
+++ internal/rules/config.go
@@ -15,17 +15,17 @@ type RulesConfig struct {
 
 var rulesConfig RulesConfig
 
-func LoadRulesConfig(filepath string) error {
+func LoadRulesConfig(filepath string) (RulesConfig, error) {
 	log.Printf("Loading rules config file %s", filepath)
 	rulesBytes, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return err
+		return RulesConfig{}, err
 	}
 
 	var rawConfig map[string]interface{}
 	err = yaml.Unmarshal(rulesBytes, &rawConfig)
 	if err != nil {
-		return err
+		return RulesConfig{}, err
 	}
 
 	config := &mapstructure.DecoderConfig{
@@ -35,14 +35,14 @@ func LoadRulesConfig(filepath string) error {
 
 	decoder, err := mapstructure.NewDecoder(config)
 	if err != nil {
-		return err
+		return RulesConfig{}, err
 	}
 
 	err = decoder.Decode(rawConfig)
 	if err != nil {
-		return err
+		return RulesConfig{}, err
 	}
 
 	log.Printf("Loaded %d rules", len(rulesConfig.Rules))
-	return nil
+	return rulesConfig, nil
 }
