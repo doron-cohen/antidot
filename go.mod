dc17bd050925504220f74b812d5a9fee799d5c6d
Alex Dewar <a.dewar@sussex.ac.uk>
2020-12-15 17:27:12 +0000

    Fix migration when dest is on different partition
    os.Rename will fail if the destination file/folder is on a different
    partition from the source. Unfortunately, in this case the only option
    is to copy the file or each of the folder's files then delete the
    original. Update migrate.go to handle this case correctly.
    
    For the folder-copying case, I have used this library here: https://github.com/otiai10/copy

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/adrg/xdg v0.2.3
 	github.com/google/go-cmp v0.5.4
 	github.com/mitchellh/mapstructure v1.4.0
+	github.com/otiai10/copy v1.3.0
 	github.com/spf13/cobra v1.1.1
 	github.com/wzshiming/ctc v1.2.3
 	gopkg.in/yaml.v2 v2.4.0
--- internal/rules/migrate.go
+++ internal/rules/migrate.go
@@ -3,17 +3,77 @@ package rules
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 
 	"github.com/doron-cohen/antidot/internal/tui"
 	"github.com/doron-cohen/antidot/internal/utils"
+	"github.com/otiai10/copy"
 )
 
 type Migrate struct {
 	Source  string
 	Dest    string
 	Symlink bool
+}
+
+func MoveDirectory(source, dest string) error {
+	err := copy.Copy(source, dest)
+	if err != nil {
+		return err
+	}
+
+	err = os.RemoveAll(source)
+	if err != nil {
+		tui.Warn("Failed to remove original directory: %s", err)
+	}
+
+	return nil
+}
+
+// Adapted from https://gist.github.com/var23rav/23ae5d0d4d830aff886c3c970b8f6c6b
+func MoveFile(source, dest string) error {
+	inputFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	outputFile, err := os.Create(dest)
+	if err != nil {
+		inputFile.Close()
+		return err
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return err
+	}
+
+	// The copy was successful, so now delete the original file
+	err = os.Remove(source)
+	if err != nil {
+		tui.Warn("Failed to remove original file: %s", err)
+	}
+
+	return nil
+}
+
+// Try to move file/directory with os.Rename and if that fails, do a copy + delete
+func Move(source, dest string) error {
+	err := os.Rename(source, dest)
+	if err == nil {
+		return nil
+	}
+
+	fi, err := os.Stat(source)
+	if fi.Mode().IsDir() {
+		return MoveDirectory(source, dest)
+	}
+
+	return MoveFile(source, dest)
 }
 
 func (m Migrate) Apply() error {
@@ -37,7 +97,7 @@ func (m Migrate) Apply() error {
 		return err
 	}
 
-	err = os.Rename(source, dest)
+	err = Move(source, dest)
 	if err != nil {
 		return err
 	}
